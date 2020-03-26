057f5a154eb86d12bbc021047353e122aec2221a
Julian David <anlijudavid@hotmail.com>
2020-03-26 18:50:41 -0500

    add :ant:

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import SOCIAL from "../constants/social";
+import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
 
@@ -20,6 +21,21 @@ const headerItems = [
 ];
 
 const Layout = ({ location, title, children }) => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      avatar: file(absolutePath: { regex: "/smallant.png/" }) {
+        childImageSharp {
+          fixed(width: 26, height: 26) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  const { avatar } = data;
+
   // const rootPath = `${__PATH_PREFIX__}/`
   let header =
     <div className="layout-header">
@@ -54,6 +70,21 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(3 / 4)}`,
       }}
     >
+      <Image
+        fixed={avatar.childImageSharp.fixed}
+        alt='ant'
+        style={{
+          marginBottom: 0,
+          minWidth: 26,
+          minHeight: 26,
+          position: 'fixed',
+          top: '1rem',
+          left: '1rem',
+          zIndex: 1000,
+          margin: 0,
+          userSelect: 'none'
+        }}
+      />
       <header>{header}</header>
       <main>{children}</main>
       <footer className="layout-footer">
