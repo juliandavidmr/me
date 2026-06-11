c950102540f0bc306ceab64f497a8f1c9962d2ec
Julian David Mora Ramos <julian.mora@crehana.com>
2026-06-10 22:49:49 -0500

    chore: update dependencies and improve code structure
    - Updated various dependencies in package.json for better performance and compatibility.
    - Refactored index.astro to use eager loading for posts.
    - Simplified post fetching in [...page].astro using eager loading.
    - Enhanced RSS feed generation in rss.xml.ts with context support.
    - Adjusted Navbar component for consistent styling.
    - Cleaned up HTML attributes in Base.astro for consistency.
    - Updated slot usage in BasePost.astro for better readability.
    - Changed module resolution in tsconfig.json to 'bundler' for improved compatibility.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,13 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import type { APIRoute } from 'astro';
 
 import { AppConfig } from '@/utils/AppConfig';
 
-export const get = () =>
+export const GET: APIRoute = async (context) =>
   rss({
-    // `<title>` field in output xml
     title: AppConfig.title,
-    // `<description>` field in output xml
     description: AppConfig.description,
-    // base URL for RSS <item> links
-    // SITE will use "site" from your project's astro.config.
-    site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
-    items: import.meta.glob('./**/*.md'),
-    // (optional) inject custom xml
+    site: context.site!,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => (
         <Logo
           icon={
             <svg
-              className="mr-1 h-10 w-10 stroke-cyan-600"
+              className="mr-1 size-10 stroke-cyan-600"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
