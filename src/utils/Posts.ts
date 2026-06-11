5e5a4514d4a327d7077748949b02181446418de8
Julian David Mora Ramos <julian.mora@crehana.com>
2026-06-10 22:52:57 -0500

    chore: update package dependencies and add overrides for components

--- src/utils/Posts.ts
+++ src/utils/Posts.ts
@@ -7,6 +7,6 @@ export const sortByDate = (posts: MarkdownInstance<IFrontmatter>[]) => {
   return posts.sort(
     (a, b) =>
       new Date(b.frontmatter.pubDate).valueOf() -
-      new Date(a.frontmatter.pubDate).valueOf()
+      new Date(a.frontmatter.pubDate).valueOf(),
   );
 };
